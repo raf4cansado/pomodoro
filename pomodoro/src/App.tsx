cf2afca2f6710043299608cbdb9191fcf77bb780
RAFAEL <88749354+raf4cansado@users.noreply.github.com>
2023-03-06 22:24:09 -0300

    inicio pomodoro

---
+++ pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function  App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>
+    Hello Wordl!
+   </h1>
+  )
+}
+
