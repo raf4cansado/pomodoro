d4822880641a328677761af7e379375200765623
RAFAEL <88749354+raf4cansado@users.noreply.github.com>
2023-03-07 23:16:08 -0300

    testando styled components

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
 
-export function  App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
   return (
-   <h1>
-    Hello Wordl!
-   </h1>
+    <h1>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant='primary' />
+        <Button variant='secondary' />
+        <Button variant='success' />
+        <Button variant='danger' />
+        <Button />
+
+        <GlobalStyle/>
+      </ThemeProvider>
+    </h1>
   )
 }
 
