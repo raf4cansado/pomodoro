f05835387dc3776363bfa4da28dce93fee5ace18
RAFAEL <88749354+raf4cansado@users.noreply.github.com>
2023-04-27 22:51:01 -0300

    nao sei

--- pomodoro/src/pages/Home/index.tsx
+++ pomodoro/src/pages/Home/index.tsx
@@ -1,20 +1,43 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+
 
 export function Home() {
+    const validacao = zod.object({
+        task: zod.string().min(1, 'Informe a tarefa'),
+        minutesAmount: zod.number().min(1).max(60)
+    })
+
+    const { register, watch, handleSubmit, formState } = useForm( {
+        resolver: zodResolver(validacao)
+    } )
+
+    function handleCreateNewCycle (event: any)  {
+        console.log(event)
+    }
+
+    console.log(formState.errors)
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action=''>
                 <FormContainer>
 
                     <label htmlFor="task"> Vou trabalhar em</label>
-                    <TaskInput id='task' placeholder='Dê um nome para o seu projeto' ></TaskInput>
+                    <TaskInput id='task' placeholder='Dê um nome para o seu projeto'
+                    {...register('task')}
+                     />
 
-                        <datalist>
-                            
-                        </datalist>
+                    <datalist>
+
+                    </datalist>
                     <label htmlFor="minutesAmount"> durante</label>
-                    <MinutesAmountInput type='number' id="minutesAmount" placeholder='00'></MinutesAmountInput>
+                    <MinutesAmountInput type='number' id="minutesAmount" placeholder='00'
+                    {...register('minutesAmount', {valueAsNumber: true}) }
+                    />
 
                     <span>minutos</span>
 
